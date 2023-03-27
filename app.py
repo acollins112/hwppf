b99b06b0cbc665e57706db57b22f313e70e98405
Aaron Statler <aaron-statler@uiowa.edu>
2023-03-27 09:04:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,24 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-friends_dict = [
-    {"name": "Test", "flavor": "swirl", "read": "yes", "activities": "reading"}
-]
-
-
-@app.route("/", methods=["GET", "POST"])
+# homepage route
+@app.route('/')
 def index():
-    return render_template(
-        "index.html", pageTitle="Web form template", friends=friends_dict
-    )
+    return render_template('index.html')
 
-
-@app.route("/add", methods=["POST"])
+# add route
+@app.route('/add', methods=['POST'])
 def add():
-    print("inside add function")
-    if request.method == "POST":
+    name = request.form['name']
+    email = request.form['email']
+    # process the data here
+    return 'Data received: {} - {}'.format(name, email)
 
-        form = request.form
+# about route
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-        fname = form["fname"]
-        flavor = form["flavor"]
-        read = form["read"]
-        activities = form.getlist("activities")  # this is a PYthon list
-
-        print(fname)
-        print(flavor)
-        print(read)
-        print(activities)
-
-        activities_string = ", ".join(activities)  # make the Python list into a string
-
-        friend_dict = {
-            "name": fname,
-            "flavor": flavor,
-            "read": read,
-            "activities": activities_string,
-        }
-
-        print(friend_dict)
-        friends_dict.append(
-            friend_dict
-        )  # append this dictionary entry to the larger friends dictionary
-        print(friends_dict)
-        return redirect(url_for("index"))
-    else:
-        return redirect(url_for("index"))
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
